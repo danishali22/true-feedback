1de0c62266628fda59aac232c72dce9720ac793f
Danish Ali <dali27363@gmail.com>
2025-02-06 20:33:38 +0500

    Add AuthProvider context and implement sign-in page with session management

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,11 +14,15 @@ export async function middleware(request: NextRequest) {
             url.pathname.startsWith('/sign-in') || 
             url.pathname.startsWith('/sign-up') || 
             url.pathname.startsWith('/verify') || 
-            url.pathname.startsWith('/')
-        )
+            url.pathname === '/')
     ) {
         return NextResponse.redirect(new URL("/dashboard", request.url));
     }
+
+    if(!token && url.pathname.startsWith("/dashboard")){
+        return NextResponse.redirect(new URL("/sign-in", request.url));
+    }
+    return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
